f51b5d99ee54aef58800d19454a00a92aec09d4c
Jlucs <janislucs@inbox.lv>
2023-04-20 06:29:52 +0300

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "https://hughesapp-backend.vercel.app",
+  "https://hughesapp-frontend.vercel.app",
   "https://www.hughesgardner.co.uk",
   "https://hughesgardner.co.uk",
 ];
